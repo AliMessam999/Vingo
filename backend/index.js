a2774c32ebc302e88cc3787b84fd1fbe86899af2
AliMessam <120706504+AliMessam12095@users.noreply.github.com>
2026-03-02 21:25:27 +0500

    Api Setted

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,22 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+import cors from "cors";
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 import connectDb from "./config/db.js";
+
+import cookieParser from "cookie-parser";
+app.use(cookieParser());
+
+import authRouter from "./routes/auth.routes.js";
+app.use("/api/auth", authRouter);
+
+app.use(express.json());
+
 
 app.listen(port, ()=>{
     connectDb();
